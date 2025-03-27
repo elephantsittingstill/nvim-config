9eb2b952165f2d75331afebc0c860b9d5d2dfaf4
Eli Knutson <eliknut@gmail.com>
2025-03-27 13:36:38 -0500

    configured toggleterm

--- lua/dennis/plugins/toggleterm.lua
+++ lua/dennis/plugins/toggleterm.lua
@@ -1,5 +1,25 @@
 return {
 	'akinsho/toggleterm.nvim',
-	verstion = "*",
-	opts = {}
+	version = "*",
+	opts = {},
+	config = function()
+		-- toggleterm setup --
+		require("toggleterm").setup ({
+			open_mapping = [[<C-\>]],
+			direction = "float",
+			insert_mappings = false,
+			start_in_insert = true,
+			close_on_exit = true,
+		})
+
+		-- Keybind to toggle floating terminal
+		vim.keymap.set("n", "<leader>t", function()
+			require("toggleterm").toggle()
+		end, { desc = "Toggle floating terminal" })
+
+		-- Keybind to escape insert mode in terminal
+		vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]],
+		{ desc = "Exit insert mode in terminal" })
+	end,
 }
+
